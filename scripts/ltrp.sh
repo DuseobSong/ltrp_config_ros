cdf56f09a2a9c483c7fdc8b2c98476d19a3c9b6a
DuseobSong <duseob.song@outlook.kr>
2024-12-20 16:11:33 +0900

    UPDATE [2024.12.10] - ROS launch on system boot    - account: root -> user    - launch ROS, when wireless network connection is available - Topic added: sub LiDAR filter result (on debug mode)

--- scripts/ltrp.sh
+++ scripts/ltrp.sh
@@ -3,9 +3,17 @@
 source /opt/ros/noetic/setup.bash
 source /home/odroid/catkin_ws/devel/setup.bash
 
+connection_flag=$(iw wlan0 link | grep -w Connected | awk '{print $1}')
+cmpr_string="Connected"
+
+until [[ $connection_flag -eq $cmpr_string ]]; do
+    sleep 1
+    connection_flag=$(iw wlan0 link | grep -w Connected | awk '{print $1}')
+done
+
 ip_addr=$(ifconfig | grep -w inet | tail -1 | awk '{print $2}')
 
 export ROS_HOSTNAME=$ip_addr
-export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311
+export ROS_MASTER_URI=http://$ip_addr:11311
 
 roslaunch ltrp_ros ltrp_core.launch
